0ceaf506cc49296844702daf83f89b41c1aadd65
Elias Guajardo <eli.guajardo5@gmail.com>
2021-06-04 14:20:44 -0500

    loop example

---
+++ example.js
@@ -0,0 +1,10 @@
+if (condition) {
+    // code here gets executed if condition evaluates to true
+}
+
+if (condition) {
+    // code here gets executed if condition evaluates to true
+} else {
+    // code here gets executed if condition evaluates to false
+}
+
---
+++ js/loop_classwork.js
@@ -0,0 +1,17 @@
+
+
+var notDone = true; // loop
+
+while (notDone) {
+    //do something
+    notDone = confirm("Do you want to go again");
+}
+
+
+var notDone = true;
+
+do{
+    /// do stuff
+    alert("Doing stuff");
+    notDone = confirm("Again");
+}while (notDone);
