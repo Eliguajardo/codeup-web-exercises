8d721ac02af45dfd926e36ee546a1ffc2c92759c
Elias Guajardo <eli.guajardo5@gmail.com>
2021-06-07 09:49:09 -0500

    exercise break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,23 @@
 'use strict';
 
-while (true) {
-    let odd_Number = prompt("Enter a odd number between 1 and 50");
-    if ((odd_Number != 1) || (odd_Number <= 50)){
-        if (odd_Number % 2 == 0){
-            break;
-        }
-    }
+var oddNumber;
+
+while(true) {
+    oddNumber = parseInt(prompt("Enter an odd number 1 and 50"));
+     if (oddNumber % 2 != 0) {
+    break;
+ }
+} console.log("Number to skip is: " + oddNumber)
+
+
+for (let index = 0; index < 50; index++) {
+     if (index == oddNumber) {
+console.log("Yikes! Skipping number " + index);
+ continue
+ }
+    if (index % 2 !=0) {
+ console.log("Here is an odd number: " + index);
+     }
 }
 
 
