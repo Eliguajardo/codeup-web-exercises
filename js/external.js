085a7eca22fd9a6fca03cc06a0c44c6c430bd7a6
Elias Guajardo <eli.guajardo5@gmail.com>
2021-06-22 23:56:28 -0500

    external exercise

--- js/external.js
+++ js/external.js
@@ -1,4 +1,23 @@
+console.log("Hello from external JavaScript");
 
-let odd_Number = prompt("Enter a odd number");
+alert("Welcome to my Website!")
 
-alert(odd_Number)
+prompt("What is your favorite color?")
+    alert("Great, blue is my favorite color too!")
+
+var x = 3;
+prompt("how long will you be renting the little mermaid?");
+    alert(x *= 3);
+
+    var y = 5;
+    prompt("Brother Bear ?");
+    alert(y *= 3);
+
+    var z = 1;
+    prompt("days for Hercules?");
+    alert(z *= 3);
+
+
+prompt(parseFloat(x,y,z) * 3);
+
+
