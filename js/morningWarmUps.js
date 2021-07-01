c9db94df040e6db321e0dd87b6b5a43fc2d65cb7
Elias Guajardo <eli.guajardo5@gmail.com>
2021-07-01 09:29:58 -0500

    morningWarmUps

---
+++ js/morningWarmUps.js
@@ -0,0 +1,16 @@
+// TODO: Create a function named countSpaces that accepts a string and returns the total number of spaces in the string.
+
+function countSpaces(value){
+    if (value === "string"){
+        return value.length
+    }
+
+}
+
+
+
+
+
+
+
+// TODO: Add validation to function above, if the argument pass is not a string it should return false.
