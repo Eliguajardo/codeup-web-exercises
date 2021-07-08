b09d0abbf5c5980d33ca86689a5ef42f1fd5aa6d
Elias Guajardo <eli.guajardo5@gmail.com>
2021-07-08 09:52:02 -0500

    morning exercise

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -87,5 +87,67 @@ personThree.info = function(){
 
 }
 
+// July 8th 2021
+// Part 1
+// TODO: Create a function that takes an array of strings and returns an array of objects
+//  where each object has 2 properties: originalString and countOfLetters
+// Example: input: ["hello", "dave"]
+//          returns: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                   ]
+
+function arrayOfStringsToObjects (input, field) {
+    var arrayObjects = [];
+
+    for (var i=0; i < arrayStrings.length ; ++i){
+        arrayobjects.push({originalString:arrayStrings[i], lengthofOriginalString:arrayStrings[i].length};
+    }
+    return arrayObjects;
+}
+
+
+
+
+
+
+// Part 2
+// TODO: Create a function that takes the array of objects from PART 1
+//  and returns a string of all of the originalStrings concatenated with spaces in between
+// Example: input: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                  ]
+//          returns: "hello dave"
+
+function arrayOfObjectsToStrings(arrayOfObjects){
+        var results = []
+    arrayOfObjects.foreach(function (objects){
+        results.push(objects.originalString)
+    })
+}
+
+
+
+// TODO: Create a function named getTallUsers that accepts an array of objects. The objects in the array will be in the same format
+//  as PersonOne, PersonTwo, and PersonThree. The functions job is to return an array of users that have heightInInches equal to or greater than 65.
+var people = [personOne,personTwo,personThree];
+// Example: getTallUsers(people)
+// ---- returns ----> [{firstName: "silvia", lastName: "floopertan", ageInYears: 34, heightInInches: 65},
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
