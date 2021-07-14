fe21c1b221aa41e60376019e8ef0c7798ad97a88
Elias Guajardo <eli.guajardo5@gmail.com>
2021-07-13 23:25:51 -0500

    jquery exercise

---
+++ js/example konami.js
@@ -0,0 +1,45 @@
+let global = {
+
+    konami: function() {
+        var konamikeys = [38,38,40,40,37,39,37,39,66,65],
+            started = false,
+            count = 0;
+        console.log("this ran");
+        $(document).keydown(function(e){
+            var reset = function() {
+                started = false;
+                count = 0;
+                return;
+            };
+
+            let key = e.keyCode;
+
+            // Begin watching if first key in sequence was pressed.
+            if(!started){
+                if(key == 38){
+                    started = true;
+                }
+            }
+
+            // If we've started, pay attention to key presses, looking for right sequence.
+            if (started){
+
+                if (konamikeys[count] == key){
+                    count++;
+                } else {
+                    // Incorrect key, restart.
+                    reset();
+                }
+                if (count == 10){
+                    // Success!
+                    alert('Konami code entered!');
+                    reset();
+                }
+            } else {
+                reset();
+            }
+        });
+    }
+}
+
+global.konami();
